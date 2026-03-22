a6787e9d4f4791f91f4c58b9f22318544c7d261e
Richardkas-rek <richardlkas161210@gmail.com>
2026-03-22 22:21:06 +0200

    java (no doc java)

--- app/src/main/java/com/example/teast12r16/MainActivity.java
+++ app/src/main/java/com/example/teast12r16/MainActivity.java
@@ -1,18 +1,52 @@
 package com.example.teast12r16;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch sw;
+    ToggleButton tg;
+    Button btn;
+    ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sw = findViewById(R.id.mySwitch);
+        tg = findViewById(R.id.myToggle);
+        btn = findViewById(R.id.myButton);
+        layout = findViewById(R.id.mainLayout);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean s = sw.isChecked();
+                boolean t = tg.isChecked();
+
+                if (!s && !t) {
+                    layout.setBackgroundColor(Color.parseColor("#222222"));
+                }
+                else if (s && !t) {
+                    layout.setBackgroundColor(Color.parseColor("#CC6666"));
+                }
+                else if (!s && t) {
+                    layout.setBackgroundColor(Color.parseColor("#6699CC"));
+                }
+                else if (s && t) {
+                    layout.setBackgroundColor(Color.parseColor("#9966CC"));
+                }
+            }
+        });
     }
 }
